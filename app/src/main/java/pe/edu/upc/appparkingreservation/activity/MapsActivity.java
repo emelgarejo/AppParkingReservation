97a3b351a3167dc38e48d5cb23f254d5b11075d1
rnld1503 <rnld1503@gmail.com>
2016-03-25 13:18:16 -0500

    Ingegración con Google Maps

---
+++ app/src/main/java/pe/edu/upc/appparkingreservation/activity/MapsActivity.java
@@ -0,0 +1,76 @@
+package pe.edu.upc.appparkingreservation.activity;
+
+import android.app.ActionBar;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import pe.edu.upc.appparkingreservation.R;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        if (actionBar!= null)
+        actionBar.hide();
+    }
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-12.046374, -77.042793);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Lima"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+
+        LatLng parking = new LatLng(-12.046884, -77.042783);
+        //BitmapDescriptor bmd = new BitmapDescriptor();
+
+        mMap.addMarker(new MarkerOptions().position(parking)
+                .title("parking here")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_parking))
+        );
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,18));
+
+    }
+}
