19018e86e1e5733c156e5300d84e4d75e0fa86ea
Orlando Carril Ramirez <omcr1905@gmail.com>
2016-03-28 13:02:08 -0500

    Se actualizo
    Modificacion del menu

--- app/src/main/java/pe/edu/upc/appparkingreservation/activity/MainActivity.java
+++ app/src/main/java/pe/edu/upc/appparkingreservation/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.DrawerLayout.DrawerListener;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
