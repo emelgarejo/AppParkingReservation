455b70f3571c20a49e33bf70254391ed90329168
Heavyarms <daniel.juarez.ignacio@gmail.com>
2016-03-27 21:29:12 -0500

    Prueba commit. DJ

--- app/src/main/java/pe/edu/upc/appparkingreservation/activity/ParkingViewActivity.java
+++ app/src/main/java/pe/edu/upc/appparkingreservation/activity/ParkingViewActivity.java
@@ -9,5 +9,6 @@ public class ParkingViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_parking_view);
+        //setContentView(R.layout.activity_parking_view);
     }
 }
